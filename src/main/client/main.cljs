8e2988ba861ae09def499df2ad0e6f13301ac7dc
Mason Vines <mason.vines@gmail.com>
2020-07-14 19:07:51 +0000

    Adding todos.

--- src/main/client/main.cljs
+++ src/main/client/main.cljs
@@ -1,16 +1,125 @@
 (ns client.main
   (:require [cljsjs.react]
             [cljsjs.react.dom]
-            [reagent.core :as reagent :refer [atom]]
-            [reagent.dom :as rdom]))
+            [cljs.reader :as reader]
+            [clojure.string :as str]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]
+            [reagent.core :as r :refer [atom]]
+            [reagent.dom :as rdom])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defn home-page []
-  [:div
-   [:h1 "Hello World"]])
+(defonce todos (r/atom (sorted-map)))
+
+(defn af-post [url data]
+  (http/post url (assoc-in data [:headers "X-CSRF-token"] js/antiForgeryToken)))
+
+(defn parse-todo [response]
+  (:body response))
+
+(defn add-todo [text]
+  (go (let [response (<! (af-post "/todos" {:body {:title text}}))
+            updated-todos (parse-todo response)]
+        (reset! todos updated-todos))))
+
+(defn toggle [id] (swap! todos update-in [id :done] not))
+(defn save [id title] (swap! todos assoc-in [id :title] title))
+(defn delete [id] (swap! todos dissoc id))
+
+(defn mmap [m f a] (->> m (f a) (into (empty m))))
+(defn complete-all [v] (swap! todos mmap map #(assoc-in % [1 :done] v)))
+(defn clear-done [] (swap! todos mmap remove #(get-in % [1 :done])))
+
+(defonce init (do
+                (add-todo "Rename Cloact to Reagent")
+                (add-todo "Add undo demo")
+                (add-todo "Make all rendering async")
+                (add-todo "Allow any arguments to component functions")
+                (complete-all true)))
+
+(defn todo-input [{:keys [title on-save on-stop]}]
+  (let [val (r/atom title)
+        stop #(do (reset! val "")
+                  (if on-stop (on-stop)))
+        save #(let [v (-> @val str str/trim)]
+                (if-not (empty? v) (on-save v))
+                (stop))]
+    (fn [{:keys [id class placeholder]}]
+      [:input {:type "text" :value @val
+               :id id :class class :placeholder placeholder
+               :on-blur save
+               :on-change #(reset! val (-> % .-target .-value))
+               :on-key-down #(case (.-which %)
+                               13 (save)
+                               27 (stop)
+                               nil)}])))
+
+(def todo-edit (with-meta todo-input
+                 {:component-did-mount #(.focus (rdom/dom-node %))}))
+
+(defn todo-stats [{:keys [filt active done]}]
+  (let [props-for (fn [name]
+                    {:class (if (= name @filt) "selected")
+                     :on-click #(reset! filt name)})]
+    [:div
+     [:span#todo-count
+      [:strong active] " " (case active 1 "item" "items") " left"]
+     [:ul#filters
+      [:li [:a (props-for :all) "All"]]
+      [:li [:a (props-for :active) "Active"]]
+      [:li [:a (props-for :done) "Completed"]]]
+     (when (pos? done)
+       [:button#clear-completed {:on-click clear-done}
+        "Clear completed " done])]))
+
+(defn todo-item []
+  (let [editing (r/atom false)]
+    (fn [{:keys [id done title]}]
+      [:li {:class (str (if done "completed ")
+                        (if @editing "editing"))}
+       [:div.view
+        [:input.toggle {:type "checkbox" :checked done
+                        :on-change #(toggle id)}]
+        [:label {:on-double-click #(reset! editing true)} title]
+        [:button.destroy {:on-click #(delete id)} "X"]]
+       (when @editing
+         [todo-edit {:class "edit" :title title
+                     :on-save #(save id %)
+                     :on-stop #(reset! editing false)}])])))
+
+(defn todo-app [props]
+  (let [filt (r/atom :all)]
+    (fn []
+      (let [items (vals @todos)
+            done (->> items (filter :done) count)
+            active (- (count items) done)]
+        [:div
+         [:section#todoapp
+          [:header#header
+           [:h1 "todos"]
+           [todo-input {:id "new-todo"
+                        :placeholder "What needs to be done?"
+                        :on-save add-todo}]]
+          (when (-> items count pos?)
+            [:div
+             [:section#main
+              [:input#toggle-all {:type "checkbox" :checked (zero? active)
+                                  :on-change #(complete-all (pos? active))}]
+              [:label {:for "toggle-all"} "Mark all as complete"]
+              [:ul#todo-list
+               (for [todo (filter (case @filt
+                                    :active (complement :done)
+                                    :done :done
+                                    :all identity) items)]
+                 ^{:key (:id todo)} [todo-item todo])]]
+             [:footer#footer
+              [todo-stats {:active active :done done :filt filt}]]])]
+         [:footer#info
+                    [:p "Double-click to edit a todo"]]]))))
 
 (defn ^:dev/after-load start
   []
-  (rdom/render [home-page]
+  (rdom/render [todo-app]
                (.getElementById js/document "app")))
 
 (defn ^:export main []
--- src/main/server/routes.cljs
+++ src/main/server/routes.cljs
@@ -1,26 +1,44 @@
 (ns server.routes
   (:require [bidi.bidi :as bidi]
+            [cljs.reader :as reader]
             [hiccups.runtime]
             [macchiato.middleware.anti-forgery :as af]
-            [macchiato.util.response :as r])
+            [macchiato.util.response :as r]
+            [macchiato.util.request :as request])
   (:require-macros
    [hiccups.core :refer [html]]))
 
+(defonce *todos (atom {}))
+(defonce *id (atom 0))
 
 (defn home [req res raise]
-  (-> [:html
-       [:head
-        [:meta {:name "viewport"
-                :content "minimum-scale=1, initial-scale=1, width=device-width"}]]
-       [:body
-        [:div#app
-         [:h2 "Loading Radio Controller..."]
-         [:script {:src "/js/client.js"}]
-         [:script "client.main.main()"]]]]
-      (html)
+  (let [af-token af/*anti-forgery-token*]
+    (-> [:html
+         [:head
+          [:meta {:name "viewport"
+                  :content "minimum-scale=1, initial-scale=1, width=device-width"}]]
+         [:body
+          [:div#app
+           [:h2 "Loading Radio Controller..."]
+           [:script "var antiForgeryToken = '" af-token "'"]
+           [:script {:src "/js/client.js"}]
+           [:script "client.main.main()"]]]]
+        (html)
+        (r/ok)
+        (r/content-type "text/html")
+        (res))))
+
+(defn todos [req res raise]
+  (-> @*todos
       (r/ok)
-      (r/content-type "text/html")
+      (r/transit)
       (res)))
+
+(defn new-todo [req res raise]
+  (let [text (:title (reader/read-string (request/body-string req)))]
+    (swap! *id inc)
+    (swap! *todos assoc @*id {:id @*id :title text :done false})
+    (todos req res raise)))
 
 (defn not-found [req res raise]
   (-> (html
@@ -32,7 +50,9 @@
       (res)))
 
 (def routes
-  ["/" {"" {:get home}}])
+  ["/" {"" {:get home}
+        "todos" {:get todos
+                 :post new-todo}}])
 
 (defn router [req res raise]
   (if-let [{:keys [handler route-params]} (bidi/match-route* routes (:uri req) req)]
